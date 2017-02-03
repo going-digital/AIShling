2481fd2bff53ccc696e1b604ffa0b69c455049e2
Peter Knight <cathedrow@me.com>
2017-02-03 17:08:00 +0000

    Cleanup

--- aishling/ais.cpp
+++ aishling/ais.cpp
@@ -45,18 +45,15 @@ void ais_interrupt() {
   static uint16_t rx_crc;             // word for AIS payload CRC calculation
   static uint8_t rx_one_count;        // counter of 1's to identify stuff bits
   static uint8_t rx_data_byte;        // byte to receive actual package data
-  uint8_t rx_this_bit_NRZI;           // current bit for NRZI decoding
   static uint8_t rx_prev_bit_NRZI;    // previous bit for NRZI decoding
-  uint8_t rx_bit;                     // current decoded bit
   static uint8_t rx_sync_state;       // state of preamble and start flag detection
   static uint8_t rx_sync_count;       // length of valid bits in current sync sequence
 
-  // read data bit and decode NRZI
-  if (digitalRead(radio_data))        // read encoded data bit from line
-    rx_this_bit_NRZI = 1;
-  else
-    rx_this_bit_NRZI = 0;
+  uint8_t rx_this_bit_NRZI;           // current bit for NRZI decoding
+  uint8_t rx_bit;                     // current decoded bit
 
+  // read data bit and decode NRZI
+  rx_this_bit_NRZI = digitalRead(radio_data) ? 1 : 0;
   rx_bit = !(rx_prev_bit_NRZI ^ rx_this_bit_NRZI); 	// NRZI decoding: change = 0-bit, no change = 1-bit, i.e. 00,11=>1, 01,10=>0, i.e. NOT(A XOR B)
   rx_prev_bit_NRZI = rx_this_bit_NRZI;				// store encoded bit for next round of decoding
 
@@ -73,7 +70,7 @@ void ais_interrupt() {
 
     // STATE: RESET
     case PH_STATE_RESET:                            // state: reset, prepare state machine for next packet
-      rx_bitstream &= 8000;                       // reset bit-stream (but don't throw away incoming bit)
+      rx_bitstream &= 0x8000;                       // reset bit-stream (but don't throw away incoming bit)
       rx_bit_count = 0;                           // reset bit counter
       fifo_new_packet();                          // reset fifo packet
       fifo_write_byte(ph_radio_channel);          // indicate channel for this packet
@@ -82,46 +79,45 @@ void ais_interrupt() {
       break;
 
     // STATE: WAIT FOR PREAMBLE AND START FLAG
-    case PH_STATE_WAIT_FOR_SYNC:                    // state: waiting for preamble and start flag
+    case PH_STATE_WAIT_FOR_SYNC:                  // state: waiting for preamble and start flag
       rx_bit_count++;                             // count processed bits since reset
       // START OF SYNC STATE MACHINE
       switch (rx_sync_state) {
           // SYNC STATE: RESET
-          case PH_SYNC_RESET:								// sub-state: (re)start sync process
-              if (rx_bit_count > PH_SYNC_TIMEOUT)			// if we exceeded sync time out
-                  ph_state = PH_STATE_RESET;				// reset state machine, will trigger channel hop
-              else {										// else
-                  rx_sync_count = 0;						// start new preamble
-                  if (rx_bit)
-                      rx_sync_state = PH_SYNC_1;			// we started with a 1
-                  else
-                      rx_sync_state = PH_SYNC_0;			// we started with a 0
+          case PH_SYNC_RESET:                     // sub-state: (re)start sync process
+              TXLED0; RXLED0;
+              if (rx_bit_count > PH_SYNC_TIMEOUT){// if we exceeded sync time out
+                  ph_state = PH_STATE_RESET;      // reset state machine, will trigger channel hop
+              }
+              else {                              // else
+                  rx_sync_count = 0;              // start new preamble
+                  rx_sync_state = rx_bit ? PH_SYNC_1 : PH_SYNC_0;
               }
               break;
 
           // SYNC STATE: 0-BIT
-          case PH_SYNC_0:									// sub-state: last bit was a 0
-              if (rx_bit) {								// if we get a 1
-                  rx_sync_count++;							// valid preamble bit
-                  rx_sync_state = PH_SYNC_1;					// next state
-              } else {									// if we get another 0
+          case PH_SYNC_0:                        // sub-state: last bit was a 0
+              if (rx_bit) {                      // if we get a 1
+                  rx_sync_count++;               // valid preamble bit
+                  rx_sync_state = PH_SYNC_1;     // next state
+              } else {                           // if we get another 0
                   if (rx_sync_count > PH_PREAMBLE_LENGTH)	{	// if we have a sufficient preamble length
-                      rx_sync_count = 7;							// treat this as part of start flag, we already have 1 out of 8 bits (0.......)
+                      rx_sync_count = 7;  // treat this as part of start flag, we already have 1 out of 8 bits (0.......)
                       rx_sync_state = PH_SYNC_FLAG;				// next state flag detection
                   }
-                  else										// if not
-                  rx_sync_state = PH_SYNC_RESET;				// invalid preamble bit, restart preamble detection
+                  else                    // if not
+                    rx_sync_state = PH_SYNC_RESET; // invalid preamble bit, restart preamble detection
               }
               break;
 
           // SYNC STATE: 1-BIT
-          case PH_SYNC_1:									// sub-state: last bit was a 1
-              if (!rx_bit) {								// if we get a 0
-                  rx_sync_count++;							// valid preamble bit
+          case PH_SYNC_1:                   // sub-state: last bit was a 1
+              if (!rx_bit) {                // if we get a 0
+                  rx_sync_count++;          // valid preamble bit
                   rx_sync_state = PH_SYNC_0;					// next state
               } else {									// if we get another 1
                   if (rx_sync_count > PH_PREAMBLE_LENGTH)	{	// if we have a sufficient preamble length
-                      rx_sync_count = 5;							// treat this as part of start flag, we already have 3 out of 8 bits (011.....)
+                      rx_sync_count = 5;  // treat this as part of start flag, we already have 3 out of 8 bits (011.....)
                       rx_sync_state = PH_SYNC_FLAG;				// next state flag detection
                   }
                   else										// if not
@@ -142,71 +138,76 @@ void ais_interrupt() {
                       ph_state = PH_STATE_PREFETCH;				// next state: start receiving packet
                   } else										// 1 is an error
                       rx_sync_state = PH_SYNC_RESET;				// restart preamble detection
-                  }
-                  break;
               }
-              // END OF SYNC STATE MACHINE - preamble and start flag detected
               break;
+      }
+      // END OF SYNC STATE MACHINE - preamble and start flag detected
+      break;
 
-          // STATE: PREFETCH FIRST PACKET BYTE
-          case PH_STATE_PREFETCH:								// state: pre-fill receive buffer with 8 bits
-              rx_bit_count++;									// increase bit counter
-              if (rx_bit_count == 8) {						// after 8 bits arrived
-                  rx_bit_count = 0;							// reset bit counter
-                  rx_one_count = 0;							// reset counter for stuff bits
-                  rx_data_byte = 0;							// reset buffer for data byte
-                  rx_crc = 0xffff;							// init CRC calculation
-                  ph_state = PH_STATE_RECEIVE_PACKET;			// next state: receive and process packet
-                  ph_message_type = rx_bitstream >> 10;		// store AIS message type for debugging
-                  break;
-              }
-              break;											// do nothing for the first 8 bits to fill buffer
+      // STATE: PREFETCH FIRST PACKET BYTE
+      case PH_STATE_PREFETCH:								// state: pre-fill receive buffer with 8 bits
+          rx_bit_count++;									// increase bit counter
+          if (rx_bit_count == 8) {						// after 8 bits arrived
+              rx_bit_count = 0;							// reset bit counter
+              rx_one_count = 0;							// reset counter for stuff bits
+              rx_data_byte = 0;							// reset buffer for data byte
+              rx_crc = 0xffff;							// init CRC calculation
+              ph_state = PH_STATE_RECEIVE_PACKET;			// next state: receive and process packet
+              ph_message_type = rx_bitstream >> 10;		// store AIS message type for debugging
+              break;
+          }
+          break;											// do nothing for the first 8 bits to fill buffer
 
-    // STATE: RECEIVE PACKET
-    case PH_STATE_RECEIVE_PACKET:						// state: receiving packet data
-      rx_bit = rx_bitstream & 0x80;					// extract data bit for processing
+      // STATE: RECEIVE PACKET
+      case PH_STATE_RECEIVE_PACKET:						// state: receiving packet data
+        if (ph_radio_channel) {
+          RXLED1;
+        } else {
+          TXLED1;
+        }
+        rx_bit = rx_bitstream & 0x80;					// extract data bit for processing
 
-      if (rx_one_count == 5) {						// if we expect a stuff-bit..
-        if (rx_bit) {								// if stuff bit is not zero the packet is invalid
-          ph_last_error = PH_ERROR_STUFFBIT;		// report invalid stuff-bit error
-          ph_state = PH_STATE_RESET;				// reset state machine
-        } else
-          rx_one_count = 0;						// else ignore bit and reset stuff-bit counter
+        if (rx_one_count == 5) {						// if we expect a stuff-bit..
+          if (rx_bit) {								// if stuff bit is not zero the packet is invalid
+            ph_last_error = PH_ERROR_STUFFBIT;		// report invalid stuff-bit error
+            ph_state = PH_STATE_RESET;				// reset state machine
+          } else
+            rx_one_count = 0;						// else ignore bit and reset stuff-bit counter
+            break;
+          }
+
+          rx_data_byte = rx_data_byte >> 1 | rx_bit;		// shift bit into current data byte
+
+          if (rx_bit) {									// if current bit is a 1
+            rx_one_count++;								// count 1's to identify stuff bit
+            rx_bit = 1;									// avoid shifting for CRC
+          } else
+            rx_one_count = 0;							// or reset stuff-bit counter
+
+        if (rx_bit ^ (rx_crc & 0x0001))					// CCITT CRC calculation (according to Dr. Dobbs)
+          rx_crc = (rx_crc >> 1) ^ 0x8408;
+        else
+          rx_crc >>= 1;
+        if ((rx_bit_count & 0x07)==0x07) {				// every 8th bit.. (counter started at 0)
+          fifo_write_byte(rx_data_byte);				// add buffered byte to FIFO
+          rx_data_byte = 0;							// reset buffer
+        }
+        rx_bit_count++;									// count valid, de-stuffed data bits
+        if ((rx_bitstream & 0xff00) == 0x7e00) {		// if we found the end flag 0x7e we're done
+          if (rx_crc != 0xf0b8)						// if CRC verification failed
+            ph_last_error = PH_ERROR_CRC;			// report CRC error
+          else {
+            fifo_commit_packet();					// else commit packet in FIFO
+          }
+          ph_state = PH_STATE_RESET;					// reset state machine
           break;
         }
-
-        rx_data_byte = rx_data_byte >> 1 | rx_bit;		// shift bit into current data byte
-
-        if (rx_bit) {									// if current bit is a 1
-          rx_one_count++;								// count 1's to identify stuff bit
-          rx_bit = 1;									// avoid shifting for CRC
-        } else
-          rx_one_count = 0;							// or reset stuff-bit counter
-
-      if (rx_bit ^ (rx_crc & 0x0001))					// CCITT CRC calculation (according to Dr. Dobbs)
-        rx_crc = (rx_crc >> 1) ^ 0x8408;
-      else
-        rx_crc >>= 1;
-      if ((rx_bit_count & 0x07)==0x07) {				// every 8th bit.. (counter started at 0)
-        fifo_write_byte(rx_data_byte);				// add buffered byte to FIFO
-        rx_data_byte = 0;							// reset buffer
-      }
-      rx_bit_count++;									// count valid, de-stuffed data bits
-      if ((rx_bitstream & 0xff00) == 0x7e00) {		// if we found the end flag 0x7e we're done
-        if (rx_crc != 0xf0b8)						// if CRC verification failed
-          ph_last_error = PH_ERROR_CRC;			// report CRC error
-        else {
-          fifo_commit_packet();					// else commit packet in FIFO
+        if (rx_bit_count > 1020) {						// if packet is too long, it's probably invalid
+          ph_last_error = PH_ERROR_NOEND;				// report error
+          ph_state = PH_STATE_RESET;					// reset state machine
+          break;
         }
-        ph_state = PH_STATE_RESET;					// reset state machine
         break;
-      }
-      if (rx_bit_count > 1020) {						// if packet is too long, it's probably invalid
-        ph_last_error = PH_ERROR_NOEND;				// report error
-        ph_state = PH_STATE_RESET;					// reset state machine
-        break;
-      }
-      break;
   }
   // END OF PACKET HANDLER STATE MACHINE
 
@@ -215,6 +216,7 @@ void ais_interrupt() {
     radio_rx(ph_radio_channel);    // initiate channel hop
   }
 }
+
 void ais_print_state() {
   switch (ph_state){
     case PH_STATE_OFF:
